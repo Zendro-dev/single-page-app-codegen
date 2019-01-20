c7539ac01416c4490d30d0f8a7597b4304579a1f
JKolokoltsev <j.kolokoltsev@gmail.com>
2019-01-20 01:01:44 -0600

    All three docker containers created

---
+++ test/integration-tests-mocha.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai')
+
+describe(
+    'Check functionality',
+    function() {
+
+        it('01. check', function () {
+            expect(0).equal(0);
+        });
+    });
