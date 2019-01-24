1747df4feeba663132681fa24ba279b9ec36c0b3
JKolokoltsev <j.kolokoltsev@gmail.com>
2019-01-24 07:19:33 -0600

    Web interface tests added

--- test/integration-tests-mocha.js
+++ test/integration-tests-mocha.js
@@ -26,8 +26,8 @@ before (async function () {
 
 // close all
 after (async function () {
-    await page.close();
-    browser.close();
+    //await page.close();
+    //browser.close();
 });
 
 
@@ -61,7 +61,7 @@ describe( 'Basic functionality', function() {
 
         await Promise.all([
             page.click('button[name="submit"]'),
-            page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+            page.waitForNavigation({ waitUntil: 'domcontentloaded' })
         ]);
 
         await delay(500);
@@ -69,6 +69,124 @@ describe( 'Basic functionality', function() {
         expect(await page.title()).to.eql('Reconstruct DB');
 
     });
+
+
+    it('03. Individual table is empty', async function () {
+
+        const SELECTOR = 'a[href="/individuals"]';
+        await page.waitFor(SELECTOR);
+
+        await Promise.all([
+            page.click(SELECTOR),
+            page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+        ]);
+
+        await delay(500);
+
+        expect(await page.$('td.vuetable-empty-result') !== null).to.eql(true);
+
+    });
+
+
+    it('04. Add individual', async function () {
+
+        const SELECTOR = 'a[href="/individual"] > button';
+        await page.waitFor(SELECTOR);
+
+        await Promise.all([
+            page.click(SELECTOR),
+            page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+        ]);
+
+        await delay(500);
+
+        await page.type('#individual-name-div > input', 'First Individual');
+        await page.type('#individual-transcript_counts-div > div > div > div > input', '5');
+        await page.click('button[type="submit"]');
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+
+        await delay(500);
+
+        expect(await page.$('td.right.aligned') !== null).to.eql(true);
+
+    });
+
+
+    it('05. Update individual', async function () {
+
+        let SELECTOR = 'div.custom-actions > a > button';
+        await page.waitFor(SELECTOR);
+
+        await Promise.all([
+            page.click(SELECTOR),
+            page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+        ]);
+        await delay(500);
+
+        SELECTOR = '#individual-name-div > input';
+        await page.click(SELECTOR, {clickCount: 3});
+        await page.type(SELECTOR, 'First Updated', {delay: 20});
+
+        await Promise.all([
+            page.click('button[type="submit"]'),
+            page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+        ]);
+        await delay(500);
+
+        const cell = await page.$('tbody > tr > td:nth-child(0n+2)');
+        const text = await page.evaluate(cell => cell.textContent, cell);
+
+        expect(text).to.eql('First Updated');
+
+    });
+
+    it('06. Add one more and find both', async function () {
+
+        const SELECTOR = 'a[href="/individual"] > button';
+        await page.waitFor(SELECTOR);
+
+        await Promise.all([
+            page.click(SELECTOR),
+            page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+        ]);
+
+        await delay(500);
+
+        await page.type('#individual-name-div > input', 'First Individual');
+        await page.type('#individual-transcript_counts-div > div > div > div > input', '5');
+
+
+        await Promise.all([
+            page.click('button[type="submit"]'),
+            page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+        ]);
+        await delay(500);
+
+        await page.type('input', 'First');
+        await page.click('div.inline.field > button');
+        await delay(500);
+
+        console.log("check array");
+
+        let rows = await page.$$eval('tbody > tr', row => row);
+        expect(rows.length).to.eql(2);
+    });
+
+
+    it('07. Delete all', async function () {
+
+        let rows = await page.$$eval('tbody > tr', row => row);
+
+        for(let i = 0; i < rows.length; i = i+1){
+            await page.click('div.custom-actions > button:nth-child(0n+3)');
+        }
+
+        rows = await page.$$eval('tbody > tr', row => row);
+
+        expect(rows.length).to.eql(0);
+    });
+
+
 });
 
 //hint: document.querySelector() to find correct selector
