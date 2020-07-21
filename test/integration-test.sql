1f302afa056c25b076a79e0ea066e119c53fbd95
Francisco Ramírez <franciscojrp07@gmail.com>
2020-07-20 19:09:12 -0500

    i57 - acl refactoring

--- test/integration-test.sql
+++ test/integration-test.sql
@@ -6,17 +6,49 @@ WHERE NOT EXISTS (
 );
 
 INSERT INTO roles(name)
-SELECT 'admin'
+SELECT 'administrator'
 WHERE NOT EXISTS (
-  SELECT id FROM roles WHERE name = 'admin'
+  SELECT id FROM roles WHERE name = 'administrator'
+);
+
+INSERT INTO roles(name)
+SELECT 'reader'
+WHERE NOT EXISTS (
+  SELECT id FROM roles WHERE name = 'reader'
+);
+
+INSERT INTO roles(name)
+SELECT 'editor'
+WHERE NOT EXISTS (
+  SELECT id FROM roles WHERE name = 'editor'
 );
 
 INSERT INTO role_to_users ("userId", "roleId")
 SELECT (SELECT id FROM users WHERE email = 'admin@zen.dro' AND password = '$2b$10$JaV2B19HT19FF/UeLomHxe5ohrvXCWTIRQjuF7yzz5TbnlOqO.HKa'), 
-       (SELECT id FROM roles WHERE name = 'admin')
+       (SELECT id FROM roles WHERE name = 'administrator')
 WHERE NOT EXISTS (
   SELECT id FROM role_to_users 
   WHERE 
     "userId" = (SELECT id FROM users WHERE email = 'admin@zen.dro' AND password = '$2b$10$JaV2B19HT19FF/UeLomHxe5ohrvXCWTIRQjuF7yzz5TbnlOqO.HKa') AND
-    "roleId" = (SELECT id FROM roles WHERE name = 'admin')
+    "roleId" = (SELECT id FROM roles WHERE name = 'administrator')
+);
+
+INSERT INTO role_to_users ("userId", "roleId")
+SELECT (SELECT id FROM users WHERE email = 'admin@zen.dro' AND password = '$2b$10$JaV2B19HT19FF/UeLomHxe5ohrvXCWTIRQjuF7yzz5TbnlOqO.HKa'), 
+       (SELECT id FROM roles WHERE name = 'reader')
+WHERE NOT EXISTS (
+  SELECT id FROM role_to_users 
+  WHERE 
+    "userId" = (SELECT id FROM users WHERE email = 'admin@zen.dro' AND password = '$2b$10$JaV2B19HT19FF/UeLomHxe5ohrvXCWTIRQjuF7yzz5TbnlOqO.HKa') AND
+    "roleId" = (SELECT id FROM roles WHERE name = 'reader')
+);
+
+INSERT INTO role_to_users ("userId", "roleId")
+SELECT (SELECT id FROM users WHERE email = 'admin@zen.dro' AND password = '$2b$10$JaV2B19HT19FF/UeLomHxe5ohrvXCWTIRQjuF7yzz5TbnlOqO.HKa'), 
+       (SELECT id FROM roles WHERE name = 'editor')
+WHERE NOT EXISTS (
+  SELECT id FROM role_to_users 
+  WHERE 
+    "userId" = (SELECT id FROM users WHERE email = 'admin@zen.dro' AND password = '$2b$10$JaV2B19HT19FF/UeLomHxe5ohrvXCWTIRQjuF7yzz5TbnlOqO.HKa') AND
+    "roleId" = (SELECT id FROM roles WHERE name = 'editor')
 );
