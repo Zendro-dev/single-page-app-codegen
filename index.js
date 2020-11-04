8d00658e9995667e337a75a0e8dd34aba86c7182
David Velasco <davvelsan@gmail.com>
2020-11-04 19:22:35 +0100

    feat: add -S flag to configure a ZendroStudio route

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ program
   .option('-D, --createBaseDirs', 'Try to create base directories if they do not exist.')
   .option('-P, --genPlotlyForAll', 'Generates a Plotly JS React component for all the json model files given as input.'+
     'Plotly components will be part of the SPA being generated.')
+  .option('-S, --enableZendroStudio', 'Generate a main Zendro Studio component.')
   .option('-V, --verbose', 'Show details about the results of running code generation process.');
 
 /**
@@ -33,16 +34,16 @@ program
     'If neither -f nor -A options are given, then -s option will be taken as default.')
   .option('-A, --spaDir <spaDirectory>', 'Plotly components will be generated as part of the SPA located in the given directory.'+
     'This option will be ignored if -f option is given, and the Plotly components will be part of the SPA being generated.')
-  .option('-s, --standalonePlotly [plotlyOutputDir]', 
+  .option('-s, --standalonePlotly [plotlyOutputDir]',
     'Plotly components will be generated in standalone mode in the given output directory, or in ./ if no output directory is given.'+
     'This option will be ignored if either -f option or -A option are given.')
   .action((jsonModelFile, moreJsonModelFiles, cmdObj) => {
     //flag
     genPlotly = true;
-    
+
     //arg: <jsonModelFile>
     modelsWithPlotly.push(jsonModelFile);
-    
+
     //args: [moreJsonModelFiles...]
     if(moreJsonModelFiles) {
       moreJsonModelFiles.forEach((jFile) => {
@@ -51,12 +52,12 @@ program
         }
       });
     }
-    
+
     //op: spaDir
     if(cmdObj.spaDir) {
       spaPlotlyDir = path.resolve(cmdObj.spaDir);
     } else {
-      
+
       //op: standalonePlotly
       if(cmdObj.standalonePlotly) {
         if(typeof cmdObj.standalonePlotly === 'string') {
@@ -82,12 +83,12 @@ program.parse(process.argv);
  * Case: No SPA (no input json files for SPA).
  */
 if(!program.jsonFiles){
-  
+
   //op: verbose
   let verbose = program.verbose !== undefined ? true : false;
 
   /**
-   * Check: sub-command: genPlotly 
+   * Check: sub-command: genPlotly
    */
   if(genPlotly) {
     if(spaPlotlyDir) {
@@ -145,7 +146,7 @@ if(!program.jsonFiles){
         process.exit(1);
       });
     }
-  }//end: sub-command: genPlotly 
+  }//end: sub-command: genPlotly
 
   /**
    * No sub-commands neither.
