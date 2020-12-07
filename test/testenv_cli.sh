b9d5a79381b9877f9dca2e5c6f49612beadce8ea
David Velasco <davvelsan@gmail.com>
2020-12-07 15:19:35 +0100

    chore: add test-integration CLI options

--- test/testenv_cli.sh
+++ test/testenv_cli.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# - To improve debugging and readability, each command if fully self-contained.
+# - Order of execution matters. For option compatibility, please read the manual.
 
 # Exit on error
 set -e
@@ -66,9 +69,77 @@ if [ $# -gt 0 ]; then
 fi
 
 
-# EXECUTION
-# - To improve debugging and readability, each command if fully self-contained.
-# - Order of execution matters. For option compatibility, please read the manual.
+# SHOW MANUAL
+# 1. Show the manual page for this command-line interface
+if [[ $OPT_SHOW_MANUAL == "true"  ]]; then
+  man "${TEST_DIR}/testenv_cli.man"
+  exit 0
+fi
+
+
+# PERFORM A FULL CLEAN-UP
+# 1. Remove docker containers, images, and volumes
+# 2. Remove the testing environment
+if [[ $OPT_CLEAN_UP == "true" ]]; then
+  bash "${TEST_DIR}/testenv_remove.sh"
+  exit 0
+fi
+
+
+# RUN CODE GENERATOR
+# 1. Run the code generator and apply patches
+if [[ $OPT_GEN_CODE == "true" ]]; then
+  bash "${TEST_DIR}/testenv_generate_code.sh"
+  exit 0
+fi
+
+
+# RESTART DOCKER ENVIRONMENT
+# 1. Stop docker containers and remove anonymous volumes
+# 2. Re-start the docker containers
+if [[ $OPT_RESTART_DOCKER == "true" ]]; then
+  docker-compose -f "${TEST_DIR}/integration_test_misc/docker-compose-test.yml" down -v
+  bash "${TEST_DIR}/testenv_docker_up.sh"
+  exit 0
+fi
+
+
+# RUN INTEGRATION TESTS
+# 1. Run the integration tests
+# 2. Perform a full cleanup (optionally disabled)
+if [[ $OPT_RUN_TESTS == "true" ]]; then
+  mocha "${TEST_DIR}/integration-tests-mocha.js"
+
+  # 1. Remove docker containers, images, and volumes
+  # 2. Remove the testing environment
+  if [[ -z $OPT_KEEP_RUNNING ]]; then
+    bash "${TEST_DIR}/testenv_remove.sh"
+  fi
+
+  exit 0
+fi
+
+
+# RUN CODE GENERATOR AND INTEGRATION TESTS
+# 1. Down docker services and remove anonymous volumes
+# 2. Run the code generator and apply patches
+# 3. Up the docker services
+# 4. Run integration tests
+# 5. Perform a full cleanup (optionally disabled)
+if [[ $OPT_GENCODE_RUNTESTS == "true" ]]; then
+  docker-compose -f "${TEST_DIR}/integration_test_misc/docker-compose-test.yml" down -v
+  bash "${TEST_DIR}/testenv_generate_code.sh"
+  bash "${TEST_DIR}/testenv_docker_up.sh"
+  mocha "${TEST_DIR}/integration-tests-mocha.js"
+
+  # 1. Remove docker containers, images, and volumes
+  # 2. Remove the testing environment
+  if [[ -z $OPT_KEEP_RUNNING ]]; then
+    bash "${TEST_DIR}/testenv_remove.sh"
+  fi
+
+  exit 0
+fi
 
 
 # PERFORM A COMPLETE RUN
