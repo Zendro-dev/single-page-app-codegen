ea0f7e3154d3d4f2188938451f2c779afe71f9fb
David Velasco <davvelsan@gmail.com>
2020-12-07 15:19:03 +0100

    fix: DOCKER_UID constant not set on the remove script

--- test/testenv_remove.sh
+++ test/testenv_remove.sh
@@ -8,6 +8,7 @@ SCRIPT_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 source "${SCRIPT_DIR}/testenv_constants.sh"
 
 # Remove docker containers, images, and volumes
+export DOCKER_UID="$DOCKER_UID"
 docker-compose -f "${TEST_DIR}/integration_test_misc/docker-compose-test.yml" down -v --rmi all
 
 # Remove testing environment
